276f0a604914d09d0d2ca8695cd970617b20ad44
JamesT <jmthomp1@ualberta.ca>
2021-12-10 22:28:01 -0700

    2

--- HW/main.py
+++ HW/main.py
@@ -214,7 +214,7 @@ def printResults(obj,offlineMax,offlineSol):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #part1()
-    #part2()
-    #part2Mod()
+    part1()
+    part2()
+    part2Mod()
     MD.part3main(2)
